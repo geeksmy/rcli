27c628b0785ad9fdc3d35c786acbb027d1a46f32
Kevin Ma <kevin_ma@ycsoftwares.com>
2025-04-04 10:55:00 +0800

    feat: 通过tower_http生成文件服务器

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -7,6 +7,7 @@ use serde::Deserialize;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
+use tower_http::services::ServeDir;
 use tracing::info;
 
 #[derive(Debug, Deserialize)]
@@ -17,8 +18,15 @@ struct HttpServeState {
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     info!("Serving {:?} on port {}", path, port);
 
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
+    let dir_serve = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_gzip()
+        .precompressed_zstd();
     let router = Router::new()
+        .nest_service("/tower", dir_serve)
         .route("/{*path}", get(file_handler))
         .with_state(Arc::new(state));
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
@@ -41,5 +49,20 @@ async fn file_handler(
             Ok(content) => (StatusCode::OK, content),
             Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
